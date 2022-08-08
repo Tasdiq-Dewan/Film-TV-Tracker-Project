062e6d470b6c26c15604b4ce70b6c286f9b0dba2
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-08 11:48:05 +0100

    created spring boot project

---
+++ src/main/java/com/qa/filmtvtracker/main/FilmTvTrackerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.filmtvtracker.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmTvTrackerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmTvTrackerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/filmtvtracker/main/FilmTvTrackerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.filmtvtracker.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmTvTrackerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
