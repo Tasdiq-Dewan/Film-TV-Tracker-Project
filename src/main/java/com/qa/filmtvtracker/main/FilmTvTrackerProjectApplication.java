9912a34a96c9dfa61561836c628db586814f573f
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-08 16:18:23 +0100

    created entities and config packages. Started working on Film.java

--- src/main/java/com/qa/filmtvtracker/main/FilmTvTrackerProjectApplication.java
+++ src/main/java/com/qa/filmtvtracker/main/FilmTvTrackerProjectApplication.java
@@ -2,12 +2,17 @@ package com.qa.filmtvtracker.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(value = "com.qa.filmtvtracker.config")
+@EntityScan(value = "com.qa.filmtvtracker.entities")
 public class FilmTvTrackerProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FilmTvTrackerProjectApplication.class, args);
+		System.out.println("Application started");
 	}
 
 }
