1ec144cd172e0cbd56d67cfc00b6f927d10ecf20
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-09 13:04:13 +0100

    created FilmRepo and TVShowRepo

--- src/main/java/com/qa/filmtvtracker/repo/FilmRepo.java
+++ src/main/java/com/qa/filmtvtracker/repo/FilmRepo.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.qa.filmtvtracker.entities.Film;
 
 public interface FilmRepo extends JpaRepository<Film, Long> {
-	List<Film> findFilmByName(String name);
+	List<Film> findFilmByFilmName(String name);
 	List<Film> findFilmByGenre(String genre);
 	List<Film> findFilmByDirector(String director);
-	List<Film> findFilmByYear(short year);
+	List<Film> findFilmByYearRelease(short year);
 }
