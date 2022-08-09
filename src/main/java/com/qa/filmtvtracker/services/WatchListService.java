76af20b2ed9fd9faa8a1a29174143a4f47514a94
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-09 15:51:33 +0100

    made WatchListService

---
+++ src/main/java/com/qa/filmtvtracker/services/WatchListService.java
@@ -0,0 +1,13 @@
+package com.qa.filmtvtracker.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WatchListService {
+
+}
