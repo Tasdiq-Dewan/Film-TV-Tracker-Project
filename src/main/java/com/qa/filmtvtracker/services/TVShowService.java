3eefd8f73b11d4d8bcdf754a1c30b265f05aa7b7
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-09 15:46:11 +0100

    finished methods for TVShowService

--- src/main/java/com/qa/filmtvtracker/services/TVShowService.java
+++ src/main/java/com/qa/filmtvtracker/services/TVShowService.java
@@ -1,13 +1,18 @@
 package com.qa.filmtvtracker.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.qa.filmtvtracker.entities.TVShow;
 import com.qa.filmtvtracker.entities.TVShowDTO;
-import com.qa.filmtvtracker.repo.TVShowRepo;
 import com.qa.filmtvtracker.exceptions.TVShowNotFoundException;
+import com.qa.filmtvtracker.repo.TVShowRepo;
 
+@Service
 public class TVShowService {
 
 	private TVShowRepo repo;
@@ -27,5 +32,55 @@ public class TVShowService {
 	
 	public TVShow addShow(TVShow show) {
 		return this.repo.save(show);
+	}
+	
+	public List<TVShowDTO> getAllShows(){
+		return this.repo.findAll().stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	public TVShowDTO getShow(Long id) {
+		TVShow show = this.repo.findById(id).orElseThrow(TVShowNotFoundException::new);
+		return mapToDTO(show);
+	}
+	
+	public List<TVShowDTO> searchShowsByName(String name){
+		return this.repo.findTVShowByShowName(name).stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	public List<TVShowDTO> searchShowsByGenre(String genre){
+		return this.repo.findTVShowByGenre(genre).stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	public List<TVShowDTO> searchShowsByYearBegan(short year){
+		return this.repo.findTVShowByYearBegan(year).stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	public List<TVShowDTO> searchShowsByEpisodes(int episodes){
+		return this.repo.findTVShowByEpisodes(episodes).stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	public List<TVShowDTO> searchShowsBySeasons(int seasons){
+		return this.repo.findTVShowBySeasons(seasons).stream().map(this::mapToDTO).collect(Collectors.toList());
+	}
+	
+	public TVShow updateShow(Long id, TVShow show) {
+		TVShow toSave = this.repo.findById(id).orElseThrow(TVShowNotFoundException::new);
+		toSave.setShowName(show.getShowName());
+		toSave.setGenre(show.getGenre());
+		toSave.setEpisodes(show.getEpisodes());
+		toSave.setSeasons(show.getSeasons());
+		toSave.setYearBegan(show.getYearBegan());
+		toSave.setYearEnded(show.getYearBegan());
+		return this.repo.save(toSave);
+	}
+	
+	public boolean removeShow(Long id) {
+		if(!this.repo.existsById(id)){
+			throw new TVShowNotFoundException();
+		}
+		else {
+			this.repo.deleteById(id);
+			return !this.repo.existsById(id);
+		}
 	}
 }
