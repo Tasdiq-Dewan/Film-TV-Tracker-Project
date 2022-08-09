a935bfb89ad2d0aafcbbf69f9edf3e14100939f4
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-09 11:54:50 +0100

    made FilmDTO

--- src/main/java/com/qa/filmtvtracker/entities/FilmDTO.java
+++ src/main/java/com/qa/filmtvtracker/entities/FilmDTO.java
@@ -3,23 +3,35 @@ package com.qa.filmtvtracker.entities;
 import java.sql.Date;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class FilmDTO {
-
+	
+	@Getter
+	@Setter
 	private Long filmId;
 	
+	@Getter
+	@Setter
 	private String filmName;
 	
+	@Getter
+	@Setter
 	private String director;
 	
+	@Getter
+	@Setter
 	private Date yearRelease;
 	
+	@Getter
+	@Setter
 	private String genre;
 	
+	@Getter
+	@Setter
 	private int runtime;
 }
