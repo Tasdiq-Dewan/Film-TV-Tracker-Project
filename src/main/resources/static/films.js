50f5dec43bc10f1fda6068ece217874419ff0232
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-10 10:18:10 +0100

    working on film.js

--- src/main/resources/static/films.js
+++ src/main/resources/static/films.js
@@ -2,7 +2,7 @@ function showAllFilms(){
     fetch('/api/films/getAll')
     .then(res => {
         res.json().then(body => {
-            displayBody(body);
+            displayBody(JSON.stringify(body));
         })
     })
     .catch(err =>{
@@ -15,4 +15,17 @@ function displayBody(body){
     const old_tbody = document.getElementById("film-table-body")
     const new_tbody = document.createElement("film-table-body");
     old_tbody.parentNode.replaceChild(new_tbody, old_tbody);
+    for(var film in body){
+        new_tbody.appendChild(populateRow(film));
+    }
+}
+
+function populateRow(film){
+    let row = document.createElement("tr");
+    let id = document.createElement("th");
+    id.scope = "row";
+    id.innerHTML = film['filmId'];
+    row.appendChild(id);
+    
+    return row;
 }
