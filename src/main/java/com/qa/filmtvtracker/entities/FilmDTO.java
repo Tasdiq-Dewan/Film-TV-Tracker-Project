cffcb5ccf54521b0525395eae12c9fcb6da48c36
Tasdiq-Dewan <tasdiqd@gmail.com>
2022-08-11 12:50:03 +0100

    testcreate, testget, and testdelete for filmservice test

--- src/main/java/com/qa/filmtvtracker/entities/FilmDTO.java
+++ src/main/java/com/qa/filmtvtracker/entities/FilmDTO.java
@@ -1,6 +1,7 @@
 package com.qa.filmtvtracker.entities;
 
 import java.sql.Date;
+import java.util.Objects;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -34,4 +35,29 @@ public class FilmDTO {
 	@Getter
 	@Setter
 	private int runtime;
+
+	@Override
+	public String toString() {
+		return "FilmDTO [filmId=" + filmId + ", filmName=" + filmName + ", director=" + director + ", yearRelease="
+				+ yearRelease + ", genre=" + genre + ", runtime=" + runtime + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(director, filmId, filmName, genre, runtime, yearRelease);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FilmDTO other = (FilmDTO) obj;
+		return Objects.equals(director, other.director) && Objects.equals(filmId, other.filmId)
+				&& Objects.equals(filmName, other.filmName) && Objects.equals(genre, other.genre)
+				&& runtime == other.runtime && yearRelease == other.yearRelease;
+	}
 }
--- src/test/java/com/qa/filmtvtracker/services/FilmServiceTest.java
+++ src/test/java/com/qa/filmtvtracker/services/FilmServiceTest.java
@@ -4,11 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,6 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import com.qa.filmtvtracker.config.AppConfig;
 import com.qa.filmtvtracker.entities.Film;
 import com.qa.filmtvtracker.entities.FilmDTO;
+import com.qa.filmtvtracker.exceptions.FilmNotFoundException;
 import com.qa.filmtvtracker.repo.FilmRepo;
 
 @SpringBootTest(classes= {FilmService.class, FilmRepo.class, AppConfig.class})
@@ -27,9 +28,6 @@ public class FilmServiceTest {
 	
 	@MockBean
 	FilmRepo repo;
-	
-	@Autowired
-	ModelMapper mapper;
 	
 	@Test
 	public void testCreate() {
@@ -43,17 +41,49 @@ public class FilmServiceTest {
 	}
 	
 	@Test
+	public void testGetFilm() {
+		Film film = new Film(1L, "Batman", "Tim Burton", (short) 1989, "Superhero", 126);
+		Optional<Film> op = Optional.of(film);
+		FilmDTO expected = new FilmDTO(1L, "Batman", "Tim Burton", (short) 1989, "Superhero", 126);
+		Long id = 1L;
+		Mockito.when(repo.findById(id)).thenReturn(op);
+		FilmDTO response = service.getFilm(id);
+		assertEquals(expected, response);
+		Mockito.verify(this.repo, Mockito.times(1)).findById(id);
+	}
+
+	@Test
 	public void testGetAll() {
 		Film film = new Film(1L, "Batman", "Tim Burton", (short) 1989, "Superhero", 126);
 		List<Film> list = new ArrayList<Film>();
 		list.add(film);
-		FilmDTO filmdto = mapper.map(list, FilmDTO.class);
-		List<FilmDTO> listdto = new ArrayList<FilmDTO>();
-		listdto.add(filmdto);
+		FilmDTO filmdto = new FilmDTO(1L, "Batman", "Tim Burton", (short) 1989, "Superhero", 126);
+		List<FilmDTO> expected = new ArrayList<FilmDTO>();
+		expected.add(filmdto);
 		List<FilmDTO> output = service.getAllFilms();
 		
 		Mockito.when(repo.findAll()).thenReturn(list);
-		assertEquals(output, listdto);
+		assertEquals(output, expected);
 		Mockito.verify(this.repo, Mockito.times(1)).findAll();
+	}
+
+	@Test
+	public void delete() {
+		Long id = 1L;
+		Mockito.when(repo.existsById(id)).thenReturn(true, false);
+		boolean expected = true;
+		boolean response = service.removeFilm(id);
+		assertEquals(expected, response);
+		Mockito.verify(this.repo, Mockito.times(2)).existsById(id);
+		Mockito.verify(this.repo, Mockito.times(1)).deleteById(id);
+	}
+	
+	@Test
+	public void deleteIllID() {
+		Long id = 1L;
+		
+		Mockito.when(repo.existsById(id)).thenReturn(false);
+		Exception thrown = Assertions.assertThrows(FilmNotFoundException.class, ()->{service.removeFilm(id);});
+		Mockito.verify(this.repo, Mockito.times(1)).existsById(id);
 	}
 }
